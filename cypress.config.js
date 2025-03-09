b9e87245986652f75e2f78c4a483771a04b750fb
thaischagas93 <thaischagas93@gmail.com>
2025-03-09 10:48:18 -0300

    Mudança no gitignore

--- cypress.config.js
+++ cypress.config.js
@@ -1,9 +1,11 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
+  projectId: 'z49see',
   e2e: {
     experimentalSessionAndOrigin: true,
     baseUrl: 'http://lojaebac.ebaconline.art.br/',
+    video: true,
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
