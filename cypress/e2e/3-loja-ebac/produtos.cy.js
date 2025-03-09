dc1a5c5309dc8e5bb799b82f3369335fa84b9b4b
thaischagas93 <thaischagas93@gmail.com>
2025-03-09 10:13:47 -0300

    Massa de dados em lista

--- cypress/e2e/3-loja-ebac/produtos.cy.js
+++ cypress/e2e/3-loja-ebac/produtos.cy.js
@@ -12,17 +12,33 @@ describe('Funcionalidade: Produtos', () => {
         cy.get('#tab-title-description > a').should('contain' , 'Descrição')
     });
 
-    it.only('Deve buscar um produto com sucesso', () => {
+    it('Deve buscar um produto com sucesso', () => {
         let produto = 'Aether Gym Pant'
         produtosPage.buscarProduto(produto)
         cy.get('.product_title').should('contain', produto )
     });
 
     it('Deve visitar a página do produto', () => {
-        
+        produtosPage.visitarProduto('Zeppelin Yoga Pant')
+        cy.get('.product_title').should('contain', 'Zeppelin Yoga Pant' )
     });
 
     it('Deve adicionar produto ao carrinho', () => {
-        
+        let qtd = 2
+        produtosPage.buscarProduto('Ajax Full-Zip Sweatshirt')
+        produtosPage.addProdutoCarrinho('M', 'Blue', qtd)
+        cy.get('.woocommerce-message').should('contain', qtd + ' × “Ajax Full-Zip Sweatshirt” foram adicionados no seu carrinho.')
+    });
+
+    it.only('Deve adicionar produto ao carrinho buscando da massa de dados', () => {
+        cy.fixture('produtos').then(dados => {
+            produtosPage.buscarProduto(dados[0].nomeProduto)
+            produtosPage.addProdutoCarrinho(
+                dados[0].tamanho, 
+                dados[0].cor, 
+                dados[0].quantidade)
+            cy.get('.woocommerce-message').should('contain', dados[0].nomeProduto)
+
+        })
     });
 });
--- cypress/support/page-objects/produtos.page.js
+++ cypress/support/page-objects/produtos.page.js
@@ -15,15 +15,19 @@ class ProdutosPage {
         cy.get('.products > .row')
         .contains(nomeProduto)
         .click()
-
     }
 
-    visitarProduto() {
-
+    visitarProduto(nomeProduto) {
+        //cy.visit(`produtos/${nomeProduto}`)
+        const urlFormatada = nomeProduto.replace(/ /g, '-')
+        cy.visit(`produtos/${urlFormatada}`)
     }
 
-    addProdutoCarrinho() {
-
+    addProdutoCarrinho(tamanho, cor, quantidade) {
+        cy.get('.button-variable-item-' + tamanho).click()
+        cy.get(`.button-variable-item-${cor}`).click()
+        cy.get('.input-text').clear().type(quantidade)
+        cy.get('.single_add_to_cart_button').click()
     }
 }
 
